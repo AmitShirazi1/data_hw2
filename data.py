acf1277a0cb8c79d6448399af3481a3b310cbd57
Netanel Shalev <Snetanel@campus.technion.ac.il>
2022-05-17 14:38:38 +0300

    finish the add_col func

--- data.py
+++ data.py
@@ -1,3 +1,6 @@
+from pickle import TRUE
+import re
+from time import time
 import pandas as pd
 from datetime import datetime
 
@@ -18,38 +21,64 @@ def seasons_naming_function(season_idx):
     return 'winter'
 
 
-def timestamp_division(timestamp):
-    months = list()
-    hours = list()
-    days = list()
-    years = list()
+def timestamp_hour(timestamp):
+    hour_object = datetime.strptime(timestamp[0], "%d/%m/%Y %H:%M")
+    return hour_object.hour
 
-    for t in timestamp:
-        date_object = datetime.strptime(t, "%d/%m/%Y %H:%M")
-        months.append(date_object.month)
-        hours.append(date_object.hour)
-        days.append(date_object.day)
-        years.append(date_object.year)
+def timestamp_day(timestamp):
+    day_object = datetime.strptime(timestamp[0], "%d/%m/%Y %H:%M")
+    return day_object.day
 
+def timestamp_month(timestamp):
+    month_object = datetime.strptime(timestamp[0], "%d/%m/%Y %H:%M")
+    return month_object.month
 
+def timestamp_year(timestamp):
+    year_object = datetime.strptime(timestamp[0], "%d/%m/%Y %H:%M")
+    return year_object.year
 
-        """df['Hours'] = hours
-    print(df)
-    # return months, hours, days, years"""
+def holiday_weekend_param(df):
+    if(df[['is_holiday'][0]] == 0 and df[['is_weekend'][0]] == 0):
+        return 0
+    elif (df[['is_holiday'][0]] == 0 and df[['is_weekend'][0]] == 1):
+        return 1
+    elif (df[['is_holiday'][0]] == 1 and df[['is_weekend'][0]] == 0):
+        return 2
+    else:
+        return 3
+
+def calc_t_diff(df):
+    return df[['t1'][0]] - df[['t2'][0]]
 
 
 def add_new_columns(df):
     """adds columns to df and returns the new df"""
     seasons_names_array = df[["season"]].apply(seasons_naming_function, axis='columns', raw=True)
     df['season_name'] = seasons_names_array.tolist()
+    
+    split_timestamp_hour = df[["timestamp"]].apply(timestamp_hour, axis=1, raw=True)
+    df['Hour'] = split_timestamp_hour.tolist()
+    
+    split_timestamp_day = df[["timestamp"]].apply(timestamp_day, axis=1, raw=True)
+    df['Day'] = split_timestamp_day.tolist()
+    
+    split_timestamp_month = df[["timestamp"]].apply(timestamp_month, axis=1, raw=True)
+    df['Month'] = split_timestamp_month.tolist()
+    
+    split_timestamp_year = df[["timestamp"]].apply(timestamp_year, axis=1, raw=True)
+    df['Year'] = split_timestamp_year.tolist()
+    
+    holiday_weekend_array = df.apply(holiday_weekend_param, axis=1)
+    df['is_weekend_holiday'] = holiday_weekend_array.tolist()
+    
+    t_diff_array = df.apply(calc_t_diff, axis=1)
+    df['t_diff'] = t_diff_array.tolist()
 
-    """hours = df[["timestamp"]].apply(lambda t: , axis='rows', raw=True)"""
-
-    df['Dates'] = pd.to_datetime(df['timestamp']).dt.date
-    df['Time'] = pd.to_datetime(df['timestamp']).dt.time
+    """great way without apply"""
+    # df['Hour'] = pd.to_datetime(df['timestamp']).dt.hour
+    # df['Day'] = pd.to_datetime(df['timestamp']).dt.day
+    # df['Month'] = pd.to_datetime(df['timestamp']).dt.month
+    # df['Year'] = pd.to_datetime(df['timestamp']).dt.year
+    
     print(df)
-    """ df['Hour'], df['Day'], df['Month'], df['Year']
-    = hours.tolist()
-    = days.tolist()
-    df['Month'] = months.tolist()
-    = years.tolist() """
+    
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main(argv):
     df = dt.load_data(argv[1])
     dt.add_new_columns(df)
 
+    # print(df[['is_holiday'][0]])
 
 if __name__ == '__main__':
     main(sys.argv)
