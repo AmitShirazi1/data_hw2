9bd165964180caf27517f81be850dbe505f751c7
AmitShirazi1 <amit.shirazi@campus.technion.ac.il>
2022-05-19 19:45:45 +0300

    a lot of things happend

--- clustering.py
+++ clustering.py
@@ -2,7 +2,11 @@ from cProfile import label
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 np.random.seed(2)
+
+LONGEST_POSSIBLE_DISTANCE = 100
+
 
 def add_noise(data):
     """
@@ -35,25 +39,25 @@ def transform_data(df, features):
     :param features: list of 2 features from the dataframe
     :return: transformed data as numpy array of shape (n, 2)
     """
-    feature_one_list = list(df[[features[0]][0]])
-    feature_one_min = df[[features[0]][0]].min()
-    feature_one_sum = df[[features[0]][0]].sum()
 
-    feature_two_list = list(df[[features[1]][0]])
-    feature_two_min = df[[features[1]][0]].min()
-    feature_two_sum = df[[features[1]][0]].sum()
+    first_feature_values = list(df[[features[0]][0]])
+    first_feature_min = min(first_feature_values)
+    first_feature_sum = sum(first_feature_values)
 
-    for i in range(len(feature_one_list)):
-        feature_one_list[i] = (feature_one_list[i] - feature_one_min) / feature_one_sum
-        feature_two_list[i] = (feature_two_list[i] - feature_two_min) / feature_two_sum
-        
-    normlized_features_df = pd.DataFrame()
-    normlized_features_df[features[0]] = feature_one_list
-    normlized_features_df[features[1]] = feature_two_list
-    
-    return add_noise(normlized_features_df).to_numpy()
+    second_feature_values = list(df[[features[1]][0]])
+    second_feature_min = min(second_feature_values)
+    second_feature_sum = sum(second_feature_values)
+
+    for i in range(len(first_feature_values)):
+        first_feature_values[i] = (first_feature_values[i] - first_feature_min) / first_feature_sum
+        second_feature_values[i] = (second_feature_values[i] - second_feature_min) / second_feature_sum
+
+    normalized_values_array = np.array([first_feature_values, second_feature_values]).T
+
+    return add_noise(normalized_values_array)
 
 
+# need to figure out whether it's needed
 def dist(x, y):
     """
     Euclidean distance between vectors x, y
@@ -61,7 +65,7 @@ def dist(x, y):
     :param y: numpy array of size n
     :return: the euclidean distance
     """
-    distance = (np.linalg.norm(x-y))**2
+    distance = (np.linalg.norm(x - y)) ** 2
 
     return distance
 
@@ -73,13 +77,21 @@ def assign_to_clusters(data, centroids):
     :param centroids: current centroids as numpy array of shape (k, 2)
     :return: numpy array of size n
     """
-    labels = []
-    for i in data:
-        labels.append(np.argmin(np.sum((i.reshape((1, 2)) - centroids) ** 2, axis=1)))
+    number_of_registries = len(data)
+    labels = np.zeros((number_of_registries,), dtype=int)
+    for point_index in range(number_of_registries):
+        min_distance = LONGEST_POSSIBLE_DISTANCE
+        for centroid_index in range(len(centroids)):
+            distance_to_centroid = np.linalg.norm(data[point_index] - centroids[centroid_index])
+            if min_distance >= distance_to_centroid:
+                min_distance = distance_to_centroid
+                labels[point_index] = centroid_index
+
+    #    labels.append(np.argmin(np.sum((i.reshape((1, 2)) - centroids) ** 2, axis=1)))
     return labels
 
 
-def recompute_centroids(data, k, labels):
+def recompute_centroids(data, labels, k):
     """
     Recomputes new centroids based on the current assignment
     :param data: data as numpy array of shape (n, 2)
@@ -87,10 +99,21 @@ def recompute_centroids(data, k, labels):
     :param k: number of clusters
     :return: numpy array of shape (k, 2)
     """
-    centroids = []
-    for c in range(len(k)):
-        centroids.append(np.mean([data[x] for x in range(len(data)) if labels[x] == c], axis=0))
+    number_of_registries = len(data)
+    centroids = np.zeros(shape=(k, 2), dtype=float)
+    current_cluster = np.zeros(shape=(number_of_registries, 2), dtype=float)
+    for cluster_number in range(k):
+        points_in_cluster_counter = 0
+        for index in range(number_of_registries):
+            if labels[index] == cluster_number:
+                current_cluster[points_in_cluster_counter][0] = data[index][0]
+                current_cluster[points_in_cluster_counter][1] = data[index][1]
+                points_in_cluster_counter += 1
+
+        centroids[cluster_number] = np.mean(current_cluster, axis=0)
+    # centroids.append, data[x] for x in range(len(data))  if labels[x] == c], axis=0)
     return centroids
+
 
 def kmeans(data, k):
     """
@@ -101,19 +124,19 @@ def kmeans(data, k):
     * labels - numpy array of size n, where each entry is the predicted label (cluster number)
     * centroids - numpy array of shape (k, 2), centroid for each cluster.
     """
-    centroids = choose_initial_centroids(data, k)
-    compr = True
-    while(compr):
-        labels = assign_to_clusters(data, centroids)
-        prev_centorids = centroids
-        centroids = recompute_centroids(data, centroids, labels)
-        current_centroids = centroids
-        if(np.array_equal(prev_centorids, current_centroids)):
-            compr = False
-            break
-        centroids = np.array(current_centroids)
-        # check this location
-        return labels, centroids
+    current_centroids = choose_initial_centroids(data, k)
+    labels = np.zeros((len(data),), dtype=int)
+
+    stabilized = False
+    while not stabilized:
+        labels = assign_to_clusters(data, current_centroids)
+        previous_centroids = current_centroids.copy()
+        current_centroids = recompute_centroids(data, labels, k)
+
+        if np.array_equal(previous_centroids, current_centroids):
+            stabilized = True
+
+    return labels, current_centroids
 
 
 def visualize_results(data, labels, centroids, path):
@@ -125,7 +148,7 @@ def visualize_results(data, labels, centroids, path):
     :param path: path to save the figure to.
     """
     colors = np.random.uniform(15, 80, len(centroids))
-    plt.title('{0} {1}'.format("Result for kmeans witk k =", len(centroids)))
+    plt.title('{0} {1}'.format("Result for kmeans with k =", len(centroids)))
     plt.xlabel('cnt')
     plt.ylabel('hum')
     plt.scatter(data[:, 0], data[:, 1], c=labels)
@@ -133,10 +156,9 @@ def visualize_results(data, labels, centroids, path):
     plt.show()
     # plt.savefig(path)
 
-
 # def closest_centroid(data, centroids):
 #     """returns an array containing the index to the nearest centroid for each point"""
-#     distances = np.sqrt(((data - centroids[:, np.newaxis])**2).sum(axis=2))
+#     distances = np.sqrt(((data - centroids[:, np.new_axis])**2).sum(axis=2))
 #     return np.argmin(distances, axis=0)
 
 # def move_centroids(data, closest, centroids):
--- main.py
+++ main.py
@@ -5,27 +5,28 @@ import clustering as cs
 import matplotlib.pyplot as plt
 import numpy as np
 
+K_VALUES = [2, 3, 5]
+
 
 def main(argv):
     # print("Part A: ")
     features = ["cnt", "hum"]
     df = dt.load_data(argv[1])
-    
+
     # dt.add_new_columns(df)
     # dt.data_analysis(df)
-    
+
     # labels = ""
     # path = "/Users/netanelshalev/Library/CloudStorage/OneDrive-Technion/first year/ds/data_hw2"
     path = ""
     print("Part B: ")
-    new = cs.transform_data(df,features)
-    # print(new)
-    labels, centroids= cs.kmeans(new,5)
-    # print(kmean1)
-    print(np.array_str(centroids, precision=3, suppress_small=True))
-    print(cs.visualize_results(new, labels, centroids, path))
+    normalized_noised_array = cs.transform_data(df, features)
+    for k in K_VALUES:
+        print("k =", k)
+        labels, centroids = cs.kmeans(normalized_noised_array, k)
+        print(np.array_str(centroids, precision=3, suppress_small=True))
+        cs.visualize_results(normalized_noised_array, labels, centroids, path)
 
-    
-    
+
 if __name__ == '__main__':
     main(sys.argv)
